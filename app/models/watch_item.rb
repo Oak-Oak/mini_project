6c52f27b84fcfeb152be50e7953fd7d0c1bd1fdc
GARS <christian.garcia4297@gmail.com>
2024-02-17 00:51:48 +0800

    modified validations

--- app/models/watch_item.rb
+++ app/models/watch_item.rb
@@ -1,6 +1,5 @@
 class WatchItem < ApplicationRecord
     validates :title, presence: true
     validates :genre, presence: true
-    validates :completed, inclusion: { in: [true, false] }
   end
   
